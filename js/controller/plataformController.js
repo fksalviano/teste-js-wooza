3e59646f1aa7982c80cb45572eb5cda085adaab6
fksalviano <36044137+fksalviano@users.noreply.github.com>
2018-02-01 18:07:22 -0200

    Incluindo fontes no repositorio
    Subida inicial dos fontes no repositorio

---
+++ js/controller/plataformController.js
@@ -0,0 +1,66 @@
+﻿app.controller('plataformController', function PostController($scope, plataformService) {
+    $scope.plataformas = [];
+
+    $scope.plataforma = null;
+    $scope.plano = null;
+
+    //get selected Plataform with Plains by lazy load
+    $scope.get = function () {
+        $scope.plataforma = this.plataforma;
+        if (!$scope.plataforma.planos) {
+            plataformService.getPlataformPlains($scope.plataforma).success(function (data) {
+                $scope.plataforma.planos = data.planos;
+            }).error(function (data) {
+                $scope.error = "Erro ao carregar planos da plataforma! " + data.ExceptionMessage;
+            });
+        };
+        $('#viewModal').modal('show');
+    };
+
+    //get selected Plain
+    $scope.getPlain = function () {
+        $scope.plano = this.plano;
+        $('#addModel').modal('show');
+        $('#viewModal').modal('hide');
+    };
+
+    //get all Plataforms
+    $scope.getAll = function () {
+        plataformService.getPlataformsList().success(function (data) {
+            $scope.plataformas = data.plataformas;
+        }).error(function (data) {
+            $scope.error = "Erro ao carregar plataformas! " + data.ExceptionMessage;
+        });
+    };
+
+    // add Plain/Plataform
+    $scope.add = function () {
+
+        $scope.plataforma.planos = null; //clear all plains to log only selected
+        console.log('INFORMAÇÕES:')
+        //log the client informations and selected plain
+        console.log('Dados = ' + JSON.stringify($scope.cliente));
+        console.log('Plataforma Selecionada = ' + JSON.stringify($scope.plataforma));
+        console.log('Plano Selecionado = ' + JSON.stringify($scope.plano));
+
+        //reset form
+        $scope.cliente = null;
+        $('#addModel').modal('hide');
+
+    };
+
+    //Model popup events
+    $scope.showadd = function () {
+        $scope.user = null;
+        $scope.editMode = false;
+        $('#addModel').modal('show');
+    };
+
+    $scope.cancel = function () {
+        $scope.user = null;
+        $('#addModel').modal('hide');
+    }
+
+    // initialize Plataforms list
+    $scope.getAll();
+});
---
+++ js/service/plataformService.js
@@ -0,0 +1,13 @@
+﻿app.factory('plataformService', function ($http) {
+    plataformUrl = 'http://private-59658d-celulardireto2017.apiary-mock.com/';
+    return {
+        getPlataformsList: function () {
+            var url = plataformUrl + "plataformas";
+            return $http.get(url);
+        },
+        getPlataformPlains: function (plataforma) {
+            var url = plataformUrl + "planos/" + plataforma.sku;
+            return $http.get(url);
+        }
+    }
+});
